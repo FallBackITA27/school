067739507461a62c2d62a6f8b549fd8f188c9214
Leila Massanova <49619073+FallBackITA27@users.noreply.github.com>
2025-03-19 12:27:12 +0100

    fix program

--- STA-2024-2025/unnumbered/2025_03_12/es1/main.c
+++ STA-2024-2025/unnumbered/2025_03_12/es1/main.c
@@ -1,55 +1,89 @@
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <string.h>
 
-#ifdef _WIN32
-#include <conio.h>
-#else
-#define clrscr() printf("\e[1;1H\e[2J")
-#endif
+#include <windows.h>
 
+// Dev-C++ ha problemi col linker
+// e ciò mi obbliga a ridefinire
+// clrscr in modo interno.
+void ClearScreen() {
+  HANDLE                     hStdOut;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  DWORD                      count;
+  DWORD                      cellCount;
+  COORD                      homeCoords = { 0, 0 };
+
+  hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+  if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+  if (!FillConsoleOutputCharacter(
+    hStdOut,
+    (TCHAR) ' ',
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  if (!FillConsoleOutputAttribute(
+    hStdOut,
+    csbi.wAttributes,
+    cellCount,
+    homeCoords,
+    &count
+    )) return;
+
+  SetConsoleCursorPosition( hStdOut, homeCoords );
+}
+
+void randomNums() {
+    srand(time(NULL));
+    printf("%d\n", rand());
+    printf("%d\n", rand());
+}
+
+void calcCircle() {
+    float PI = 3.141592; // const
+    float radius;
+    printf("Inserisci il raggio del cerchio\n>> ");
+    scanf("%f", &radius);
+    float circumference = 2*radius*PI;
+    float area = radius*radius*PI;
+    printf("\nLa circonferenza e': %f\nL'area e': %f\n", circumference, area);
+}
 
 int main(){
     int sc;
     while (1) {
-        clrscr();
+        ClearScreen();
         printf("MENU' DELLE FUNZIONALITA'\n");
         printf("1. stampa due numeri casuali\n");
         printf("2. calcola l'area e la circonferenza di un cerchio dato il raggio\n");
-        printf("3. calcolo affinità di coppia dati i nomi\n");
+        printf("3. calcolo affinita' di coppia dati i nomi\n");
         printf("4. ESCI DAL PROGRAMMA\n");
         printf("SCELTA:\n");
         scanf("%d",&sc);
         switch (sc) {
-        case 1:
-            srand(time(NULL));
-            printf("%d\n", rand());
-            printf("%d\n", rand());
-            break;
-        case 2:
-            float PI = 3.141592; // const
-            float radius;
-            printf("Inserisci il raggio del cerchio\n>> ");
-            scanf("%f", &radius);
-            float circumference = 2*radius*PI;
-            float area = radius*radius*PI;
-            printf("\nLa circonferenza e': %f\nL'area e': %f", circumference, area);
-            break;
-        case 3:
-            // 3. calcolo affinità di coppia dati i nomi
-            break;
-        case 4:
-            goto exit;
-        default:
-            printf("VALORE ERRATO\n");
+            case 1:
+                randomNums();
+                break;
+            case 2:
+                calcCircle();
+                break;
+            case 3:
+                // 3. calcolo affinità di coppia dati i nomi
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("VALORE ERRATO\n");
         }
-        if (sc == 4) break;
 
+        fflush(stdin);
         printf("Premi enter per continuare\n");  
         getchar();
     }
-    exit:
-    return 0;
 }
